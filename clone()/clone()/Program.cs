6927a6fbc4f6a931e45102baef70664ea306a3f7
mahnoor579 <m.azmat2013@yahoo.com>
2016-03-19 01:28:54 +0000

    clone method (strings)

---
+++ clone()/clone()/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clone__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = { "I", "am", "mahnoor" };
+            string[] cloned = array.Clone() as string[];
+            Console.WriteLine(string.Join(" ", array));
+            Console.WriteLine(string.Join(" ", cloned));
+            Console.WriteLine("-------------------------------------");
+            Console.ReadKey();
+            //for changing clones 2nd element
+            cloned[2] = "student";
+            Console.WriteLine(string.Join(" ", array));
+            Console.WriteLine(string.Join(" ", cloned));
+            Console.ReadKey();
+
+        }
+    }
+}
