049bec7d645aef0a14fbbc88a60ad70f5be36856
mahnoor579 <m.azmat2013@yahoo.com>
2016-03-19 01:20:27 +0000

    compare method (strings)

---
+++ compare()/compare()/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compare__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //compare
+            string a = "Hello";
+            string b = "world";
+            int c = string.Compare(a, b);
+            Console.WriteLine(c);
+
+            c = string.CompareOrdinal(b, a);
+            Console.WriteLine(c);
+
+            c = a.CompareTo(b);
+            Console.WriteLine(c);
+            c = b.CompareTo(a);
+            Console.WriteLine(c);
+            Console.ReadKey();
+            //concat
+            string a = "hi";
+            string b = "mahnoor";
+           Console.WriteLine( string.Concat(a," ", b));
+           Console.ReadKey();
+
+
+
+        }
+    }
+}
