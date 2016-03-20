3b6e10ec370e59ca7b3a342c66db0ef043a15779
mahnoor579 <m.azmat2013@yahoo.com>
2016-03-20 07:15:37 +0000

    visual programming

---
+++ home practice/home practice/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace home_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                case 1 :
+                case 2:
+                    Console.WriteLine("your nmber is {0}", num);
+                    Console.ReadLine();
+                    break;
+               
+                   
+                    break;
+                default:
+                    Console.WriteLine("inbgasfgbsdvc");
+                    Console.ReadLine();
+                    break;
+
+            }
+        }
+    }
+}
