ab804dc6092abc8bedbfaaa6c686e027e0d8ec0f
mahnoor579 <m.azmat2013@yahoo.com>
2016-03-20 07:10:01 +0000

    Arrays(even number)

---
+++ evenbyarrays/evenbyarrays/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evenbyarrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] en = new int[3];
+            
+                Console.WriteLine("Enter Numbers");
+                for (int i=0; i<= en.Length; i++ )
+                {
+                    en[i] = Convert.ToInt32(Console.ReadLine());
+                }
+            for (int i=0; i<=en.Length; i++)
+            {
+                if (en[i]%2==0)
+                {
+                    Console.WriteLine("Number is even");
+                    Console.ReadLine();
+                }
+                else if (en[i]%2!=0)
+                {
+                    Console.WriteLine("Number is odd");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid NUMBER");
+                    Console.ReadLine();
+                }
+            }
+            
+
+
+        }
+    }
+}
