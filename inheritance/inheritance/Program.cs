d1cee4343e8791deb0e29033e2c8b8c2f9abd205
mahnoor579 <m.azmat2013@yahoo.com>
2016-03-20 07:06:53 +0000

    Inheritance

---
+++ inheritance/inheritance/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    class Program
+    {
+        class shape
+        {
+            protected int height;
+                protected int width;
+            public void setheight (int h)
+                {
+                    height = h;
+                }
+            public void setwidth (int w)
+            {
+                width = w;
+            }
+
+        }
+        class Rectangle:shape
+        {
+            public void area()
+            {
+                int a = height * width;
+                Console.WriteLine("Area of rectangle is :" + a);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int h;
+            int w;
+            Console.WriteLine("please enter height and width");
+            h = Convert.ToInt32(Console.ReadLine());
+            w = Convert.ToInt32(Console.ReadLine());
+
+
+            
+            
+            Rectangle r1 = new Rectangle();
+            r1.setheight(h);
+            r1.setwidth(w);
+            r1.area();
+            Console.Read();
+
+
+
+        }
+    }
+}
