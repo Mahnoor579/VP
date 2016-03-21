018c58268d6bff6c6e9d141f85b45c6e63e9a6c2
mahnoor579 <m.azmat2013@yahoo.com>
2016-03-21 08:24:12 +0000

    visual programming

--- Overriding/Overriding/Program.cs
+++ Overriding/Overriding/Program.cs
@@ -6,10 +6,49 @@ using System.Threading.Tasks;
 
 namespace Overriding
 {
-    class Program
+    class Employee
     {
-        static void Main(string[] args)
+        public string firstName;
+        public string secondName;
+       
+        public virtual void printName()
         {
+            Console.WriteLine(firstName + " " + secondName);
+            Console.ReadKey();
+        }
+
+        public class PartTimeEmployee : Employee
+        {
+            public override void printName()
+            {
+                Console.WriteLine(firstName + " " + secondName + "-PART TIME EMPLOYEE");
+                Console.ReadKey();
+            }
+        }
+        public class fulltimeEmployee : Employee
+        {
+            public override void printName()
+            {
+                Console.WriteLine(firstName + " " + secondName + "-FULL TIME  EMPLOYEE");
+                Console.ReadKey();
+            }
+        }
+
+        static void Main(string[] args)
+
+        {
+            
+            //fulltimeEmployee F1 = new fulltimeEmployee();
+
+            //F1.firstName = "Mahnoor";
+            //F1.secondName = "Ahmed";
+           
+            //F1.printName();
+            Employee e1 = new Employee();
+            e1.firstName = "Mahnoor";
+            e1.secondName = "Ahmed";
+            e1.printName();
+            
         }
     }
 }
