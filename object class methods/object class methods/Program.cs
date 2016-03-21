903f7a2fb1823e53df4f62aa76d136d00f586d17
mahnoor579 <m.azmat2013@yahoo.com>
2016-03-21 07:58:32 +0000

    visual programming

---
+++ object class methods/object class methods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace object_class_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Equals
+            bool b = object.Equals("a", 'a'.ToString());
+            Console.WriteLine(b);
+            Console.ReadKey();
+            //Reference Equals
+            bool a = object.ReferenceEquals("a", 'a'.ToString());
+            Console.WriteLine(a);
+            Console.ReadKey();
+
+        }
+    }
+}
