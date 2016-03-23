3333cff65f7fbda0a99835cdd3c3a7db5820485f
mahnoor579 <m.azmat2013@yahoo.com>
2016-03-23 11:55:51 +0000

    visual programming

---
+++ copy to (string)/string trim/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_trim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = " We are learning strings";
+            string str2;
+            str2 = string.Copy(str1);
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+            Console.ReadKey();
+
+           
+
+        }
+    }
+}
