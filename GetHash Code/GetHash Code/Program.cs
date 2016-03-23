bc1f0e47b4314d0222bc2c2c7f0f781baad10c26
mahnoor579 <m.azmat2013@yahoo.com>
2016-03-23 11:56:37 +0000

    VP

---
+++ GetHash Code/GetHash Code/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetHash_Code
+{
+    class Student
+    {
+        public int age = 20;
+        public virtual void print()
+        {
+            Console.WriteLine(age);
+            Console.ReadKey();
+
+        }
+        public class student2 : Student
+        {
+            public override int GetHashCode()
+            {
+                return base.GetHashCode();
+            }
+        }
+        static void Main(string[] args)
+        {
+            student2 sd2 = new student2();
+            sd2.GetHashCode();
+
+        }
+    }
+}
