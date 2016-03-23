0f4bf94394bec5115abba370ece58545640cb9dd
mahnoor579 <m.azmat2013@yahoo.com>
2016-03-23 12:29:54 +0000

    vp

---
+++ area of circle/tut 4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tutorial_1
+{
+    class Program
+    {
+        class Circle
+        {
+            double radius;
+            public void getvalue (double rad)
+            {
+                radius = rad;
+            }
+            public double area()
+            {
+                return 3.14 * radius * radius;
+            }
+            public void display ()
+            {
+                Console.WriteLine("The area of the circle is : " + area());
+               
+            }
+        }
+       
+        
+        static void Main(string[] args)
+        {
+            double rad;
+          
+            Console.WriteLine("please enter the radius:");
+            rad = Convert.ToDouble(Console.ReadLine());
+            Circle c1 = new Circle();
+            c1.getvalue(rad);
+            c1.display();
+            Console.Read();
+
+            
+        }
+    }
+}
