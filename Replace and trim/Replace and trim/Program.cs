7b730e23a2c871b960deb7f28681664130eb0916
mahnoor579 <m.azmat2013@yahoo.com>
2016-03-23 12:18:05 +0000

    VP

---
+++ Replace and trim/Replace and trim/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Replace_and_trim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //TRIM
+            string str1 = "Hello world";
+            string str2 = "Mahnoor Ahmed";
+            //str1.Trim();
+            //Console.WriteLine(str1);
+            //Console.ReadKey();
+            //replace
+            str1 = str1.Replace(" ", string.Empty);
+            Console.WriteLine(str1);
+            Console.ReadKey();
+
+        }
+       
+        
+        
+    }
+}
