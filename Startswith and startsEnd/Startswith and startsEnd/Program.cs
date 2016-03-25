8a1693c7c44a6260eccf7c85c442d13127897ba4
mahnoor579 <m.azmat2013@yahoo.com>
2016-03-25 23:38:32 +0000

    vp

---
+++ Startswith and startsEnd/Startswith and startsEnd/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Startswith_and_startsEnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             string str1 = "mahnoor";
+            if (str1.StartsWith("m"))
+            {
+                Console.WriteLine("True");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("false");
+                Console.ReadKey();
+            }
+
+            if (str1.EndsWith("r")) 
+            {
+                Console.WriteLine("True");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("True");
+                Console.ReadKey();
+            }
+          
+           
+        }
+    }
+}
