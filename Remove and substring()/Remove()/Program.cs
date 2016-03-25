9466bbdd5ed9024c46ae5c6a4b30af4c7a5638f6
mahnoor579 <m.azmat2013@yahoo.com>
2016-03-25 22:38:03 +0000

    vp

---
+++ Remove and substring()/Remove()/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remove__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Remove
+            //string str1 = "0123456";
+            //string str2 = str1.Remove(6);
+            //Console.WriteLine(str2);
+            //Console.ReadKey();
+
+
+            //substring
+            string str1 = "One Two Three";
+                string str2 = str1.Substring(4,3);
+                Console.WriteLine("substring : {0}", str2);
+                Console.ReadKey();
+
+
+        }
+    }
+}
