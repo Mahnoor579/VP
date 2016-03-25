9413ccaaf5ca13546be67c713371ddbed2b24773
mahnoor579 <m.azmat2013@yahoo.com>
2016-03-25 23:20:01 +0000

    vp

---
+++ Trimstart and TrimEnd/Trimstart and TrimEnd/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trimstart_and_TrimEnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // TRIM END (SIMPLE)
+            //string str1 = "hello!!@";
+            //string str2 = str1.TrimEnd('!','@');
+            //Console.WriteLine(str2);
+            //Console.ReadKey();
+            
+
+            //TRIM END(ARRAYS)
+           // string[] str1 = { "hi!!", "whats up?" };
+           //foreach (string str2 in str1)
+           //{
+           //    string str3 = str2.TrimEnd('!', '?');
+           //    Console.WriteLine(str3);
+           //    Console.ReadKey();
+
+
+               //TRIM START
+
+            string str1 = "@@Hello";
+            string str2 = str1.TrimStart('@');
+            Console.WriteLine(str2);
+            Console.ReadKey();
+
+
+           }
+        }
+    }
+
