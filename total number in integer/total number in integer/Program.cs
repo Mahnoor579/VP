631a09da52877e218a0b86301c81c5d80fe782dd
mahnoor579 <m.azmat2013@yahoo.com>
2016-03-26 04:01:58 +0000

    vp

---
+++ total number in integer/total number in integer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace total_number_in_integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, count = 0;
+            Console.WriteLine("Enter the number");
+            number = Convert.ToInt32(Console.ReadLine());
+            while (number != 0)
+            {
+                number = number / 10;
+                count++;
+            }
+            Console.WriteLine("Total number in the integer : {0}", count);
+            Console.ReadLine();
+        }
+    }
+}
