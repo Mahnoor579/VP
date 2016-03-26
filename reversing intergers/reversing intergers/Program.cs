da6c78893f08c9f40eb8d69727bbdfc080e8108f
mahnoor579 <m.azmat2013@yahoo.com>
2016-03-26 01:08:09 +0000

    vp

---
+++ reversing intergers/reversing intergers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reversing_intergers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, reverse = 0, remainder = 0;
+            Console.WriteLine("Enter the number");
+            number = Convert.ToInt32(Console.ReadLine());
+            while (number != 0)
+            {
+                remainder = number % 10;
+                reverse = reverse * 10 + remainder;
+                number = number / 10;
+
+            }
+            Console.WriteLine("The reverse number is : {0}", reverse);
+            Console.ReadLine();
+        }
+    }
+}
