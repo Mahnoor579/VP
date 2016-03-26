477177c088fe257156db7726d5a5e5f0456e766b
mahnoor579 <m.azmat2013@yahoo.com>
2016-03-26 22:56:42 +0000

    vp

---
+++ fabonacci/logical question/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logical_question
+{
+    class Program
+    {
+        public static int Fibonacci(int n)
+        {
+            int a = 0;
+            int b = 1;
+            
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                Console.WriteLine(Fibonacci(i));
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
