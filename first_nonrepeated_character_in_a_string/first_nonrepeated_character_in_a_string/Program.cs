2d58043f43b4e75d05feff4511cfa848223a518d
mahnoor579 <m.azmat2013@yahoo.com>
2016-03-26 00:09:36 +0000

    vp

---
+++ first_nonrepeated_character_in_a_string/first_nonrepeated_character_in_a_string/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_nonrepeated_character_in_a_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int flag = 1;
+            string str = "mahnoor";
+            char[] ch = str.ToCharArray();
+            for (int i = 0; i<ch.Length; i++)
+            {
+                flag = 1;
+                for (int j=i+1; j<ch.Length; j++)
+                {
+                    if(ch[i]==ch[j])
+                    {
+                        flag = 0;
+                        break;
+                    }
+                }
+                if (flag==1)
+                {
+                    Console.WriteLine("first non repeated character is: {0}", ch[i]);
+                    Console.ReadKey();
+                    break;
+                }
+            }
+        }
+    }
+}
