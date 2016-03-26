434b3c90a00379dff775710453255a6126f168d6
mahnoor579 <m.azmat2013@yahoo.com>
2016-03-26 00:58:34 +0000

    vp

---
+++ palindrome/palindrome/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "madam";
+            char[] ch = str.ToCharArray();
+            Array.Reverse(ch);
+            string str2 = new string(ch);
+            if (str.Equals(str2))
+            {
+                Console.WriteLine("palindrome");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("not palindrome");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
