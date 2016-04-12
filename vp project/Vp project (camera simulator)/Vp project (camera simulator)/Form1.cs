baed6c547b74ada9da83e25377c24514be749cfd
mahnoor579 <m.azmat2013@yahoo.com>
2016-04-12 13:25:42 +0100

    vp

---
+++ vp project/Vp project (camera simulator)/Vp project (camera simulator)/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vp_project__camera_simulator_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Simulator s2 = new Simulator();
+            s2.ShowDialog();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ vp project/Vp project (camera simulator)/Vp project (camera simulator)/Simulator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vp_project__camera_simulator_
+{
+    public partial class Simulator : Form
+    {
+        public Simulator()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Editor ed = new Editor();
+            ed.ShowDialog();
+        }
+    }
+}
