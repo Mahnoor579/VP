5c538db263327bbe56d8a6c6abb104887241eac1
mahnoor579 <m.azmat2013@yahoo.com>
2016-04-19 18:40:24 +0100

    VP

---
+++ Button(aftermidz)/Button(aftermidz)/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Button_aftermidz_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Button btn = new Button();
+            btn.Location = new Point(50, 70);
+            btn.Width = 120;
+            btn.Height = 30;
+            btn.Text = "PRESS";
+            this.Controls.Add(btn);
+            btn.Visible = true;
+        }
+    }
+}
