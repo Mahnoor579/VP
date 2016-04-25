9d6c8599d00daaa259ee91c59c1d0caca7374c16
mahnoor579 <m.azmat2013@yahoo.com>
2016-04-25 13:04:59 +0100

    vp

---
+++ Singleton pattern/Singleton pattern/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Singleton_pattern
+{
+    class singleton
+    {
+        singleton obj;
+        public static singleton SING
+     
+        {
+            get (if obj==null)
+        {
+            obj = new singleton();
+            return obj;
+        }
+            else 
+            return obj;
+
+        }
+
+         public string Test()
+{
+    return "singleton pattern executed";
+}   
+                
+                
+       
+    }
+   
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBox.Show(singleton.SING.Test());
+        }
+    }
+}
